e83e8086c6515bfe612801a26d058ffaef3af767
Jeonsangeon <wjstkddjs28@gmail.com>
2024-03-06 18:03:55 +0900

    init

---
+++ src/main/java/com/cosmos/container/ContainerApplication.java
@@ -0,0 +1,11 @@
+package com.cosmos.container;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContainerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ContainerApplication.class, args);
+	}
+}
