03e7a22a51e27a50068acee6c99c92d3d1bc86b0
Jeonsangeon <wjstkddjs28@gmail.com>
2024-04-15 19:23:13 +0900

    modified Product Controller

--- src/main/java/com/cosmos/container/controller/ProductController.java
+++ src/main/java/com/cosmos/container/controller/ProductController.java
@@ -3,8 +3,9 @@ package com.cosmos.container.controller;
 import com.cosmos.container.dto.ProductDTO;
 import com.cosmos.container.jwt.JWTUtil;
 import com.cosmos.container.service.ProductService;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,15 +18,14 @@ public class ProductController {
     private final JWTUtil jwtUtil;
 
     @PostMapping("/save")
-    public String saveProduct(@RequestBody ProductDTO productDTO, HttpServletRequest request){
-        String memberId = jwtUtil.getUsername(request.getHeader("access"));
-        productDTO.setMemberId(memberId);
+    public String saveProduct(@RequestBody ProductDTO productDTO, @AuthenticationPrincipal UserDetails userDetails){
+        productDTO.setMemberId(userDetails.getUsername());
         return productService.saveProduct(productDTO);
     }
 
     @GetMapping("/posts")
-    public List<ProductDTO> getProducts(HttpServletRequest request){
-        String memberId = jwtUtil.getUsername(request.getHeader("access"));
-        return productService.getProducts(memberId);
+    public List<ProductDTO> getProducts(@AuthenticationPrincipal UserDetails userDetails){
+        System.out.println(userDetails.getUsername());
+        return productService.getProducts(userDetails.getUsername());
     }
 }
