82bb60be1811fc072a5d085d22213fbcea4ae381
chan8632 <duscks8632@gmail.com>
2024-06-02 13:19:29 +0900

    팔레트 타입 및 컨테이너 리스트 서버용

--- src/main/frontend/src/pages/manager/containerList.tsx
+++ src/main/frontend/src/pages/manager/containerList.tsx
@@ -219,49 +219,32 @@ export default function ContainerList() {
       </div>
     );
   };
-  // 날짜 객체 생성
-  // const customTime = (row: any) => {
-  //   const oriDate = new Date(row.original.deadline);
 
-  //   // 날짜를 문자열로 변환하고 'GMT' 부분 앞에서 줄바꿈
-  //   const dateString = oriDate.toString();
-  //   const dateArray = dateString.split("");
-  //   const formattedDate = (
-  //     <span>
-  //       {dateArray[0]}
-  //       <br />
-  //       {dateArray[1]}
-  //     </span>
-  //   );
-
-  //   return formattedDate;
-  // };
-
-  const [data, _setData] = React.useState<Person[]>(defaultData);
+  const [data, _setData] = React.useState<Person[]>(() => []);
 
   // const [data, _setData] = React.useState<Person[]>(() => []);
   const [refeach, _setfetch] = useState(false);
   //처음에 백엔드와 데이터 통신하거나 데이터 수정됐을 때 다시 불러오는 역할
 
-  // useEffect(() => {
-  //   (async () => {
-  //     const response = await CreateAxiosInstance().get("/containers");
-  //     const list = response.data.map((list: Person) => ({
-  //       ...list,
-  //     }));
-  //     _setData(list);
-  //   })();
-  // }, []);
+  useEffect(() => {
+    (async () => {
+      const response = await CreateAxiosInstance().get("/containers");
+      const list = response.data.map((list: Person) => ({
+        ...list,
+      }));
+      _setData(list);
+    })();
+  }, []);
 
-  // useEffect(() => {
-  //   (async () => {
-  //     const response = await CreateAxiosInstance().get("/containers");
-  //     const list = response.data.map((list: Person) => ({
-  //       ...list,
-  //     }));
-  //     _setData(list);
-  //   })();
-  // }, [refeach]);
+  useEffect(() => {
+    (async () => {
+      const response = await CreateAxiosInstance().get("/containers");
+      const list = response.data.map((list: Person) => ({
+        ...list,
+      }));
+      _setData(list);
+    })();
+  }, [refeach]);
 
   const table = useReactTable({
     data,
